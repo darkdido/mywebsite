31a3ded18af7980101e0265db277afb4f61966f9
dyari <70205686+darkdido@users.noreply.github.com>
2021-04-01 01:35:33 -0700

    Add files via upload

--- db_conn.php
+++ db_conn.php
@@ -1,10 +1,10 @@
 <?php
 
-$sname= "sql103.unaux.com";
-$unmae= "unaux_28245691";
-$password = "cex29k6fj";
+$sname= "sql209.unaux.com";
+$unmae= "unaux_28248686";
+$password = "z92wgu3uzm5mo";
 
-$db_name = "unaux_28245691_users";
+$db_name = "unaux_28248686_users";
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
